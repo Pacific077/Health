8449ee8869572f3740c7f7b20b8abbdf069d2fa2
Pacific077 <2021ugcs077@nitjsr.ac.in>
2023-12-22 10:10:30 +0530

    added lock button on login page

--- frontend/src/components/Login/LoginPage.jsx
+++ frontend/src/components/Login/LoginPage.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPage.css";
+import {faLock,faLockOpen} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
 const LoginPage = () => {
+  const [showPass,setShowPass] =  useState(false);
+  const handleShowPass =()=>{
+    setShowPass(!showPass);
+  }
   return (
     <div className="loginPagebg">
       <div className="loginpage">
@@ -12,7 +18,11 @@ const LoginPage = () => {
             <label htmlFor=""  >Email</label>
             <input type="text" placeholder='abx@exxmple.com'/>
             <label htmlFor="">Password</label>
-            <input type="text" />
+            <div>
+
+            <input type={showPass ? "text" : "password"} />
+            <FontAwesomeIcon className="lockicon" icon= {showPass?faLockOpen:faLock} onClick={handleShowPass}/>
+            </div>
             <button className="signupbtn">Sign Up</button>
             <p>Dont have an Account ?</p>
           </div>
